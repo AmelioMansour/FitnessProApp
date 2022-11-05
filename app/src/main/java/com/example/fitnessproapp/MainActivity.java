b82da2da63fa4364d142b8f2ae9cfe51157f3c1d
amelio <ameliomansour@yahoo.com>
2022-11-05 17:02:40 -0400

    Setup and removed things to get webpage to be as an app.

--- app/src/main/java/com/example/fitnessproapp/MainActivity.java
+++ app/src/main/java/com/example/fitnessproapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.fitnessproapp;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -13,25 +16,24 @@ import androidx.navigation.ui.NavigationUI;
 import com.example.fitnessproapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    WebView webView;
 
-    private ActivityMainBinding binding;
+
+    //private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // webview section made from vid
+        setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://ameliomansour.github.io/Fitness-Pro-Website/Frontend");
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+
     }
 
 }
